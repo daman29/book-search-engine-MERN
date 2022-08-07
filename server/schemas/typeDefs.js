6da8e66e7af017dcf032e3e67362cf59aba5e61a
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-07 20:56:19 +0930

    added save and remove book mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -28,12 +28,18 @@ const typeDefs = gql`
   }
 
   input BookInput {
+    authors: [String]
+    description: String!
+    image: String
+    link: String
+    title: String
   }
 
   type Mutation {
     login(email: String!, password: String!): Auth
     addUser(username: String!, email: String!, password: String!): Auth
-    saveBook: 
+    saveBook(input: BookInput): User
+    removeBook(bookId: String!): User
   }
 `;
 
