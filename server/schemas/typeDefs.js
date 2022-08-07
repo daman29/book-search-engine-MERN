5ff6b06dd5322dad1a9e4ca17dfbd2eb69e9d30c
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-07 17:09:58 +0930

    added bookSchema typeDef

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -3,10 +3,20 @@ const { gql } = require("apollo-server-express");
 const typeDefs = gql`
     type User {
         _id: ID
-        username: String
-        email: String
-        password: String
+        username: String!
+        email: String!
+        password: String!
         savedBooks: [bookSchema]
+    }
+
+    type bookSchema {
+        _id: ID
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String
     }
 `;
 
