ed0f047812032373e5dc9dd3bbdcdd276e7a46ae
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-07 21:01:19 +0930

    added me query to resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,15 @@
+const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models/index.js");
 
-const resolvers = {};
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError("You need to be logged in!");
+    },
+  },
+};
 
 module.exports = resolvers;
