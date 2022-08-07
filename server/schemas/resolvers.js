41820ccd053df4add467e3b3de7cb07c173f48b6
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-08-07 21:08:32 +0930

    added addUser Mutation to save token and return Auth object

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models/index.js");
+const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
@@ -12,10 +13,12 @@ const resolvers = {
   },
 
   Mutation: {
-    addUser: async(parent, {username, email, password}) => {
-        const user = await User.create({username, email, password});
-    }
-  }
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
